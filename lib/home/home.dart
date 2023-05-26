392d79db9c992c65a6e77cc90073d16c48c0d386
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2023-05-26 17:06:27 +0800

    Appbar

---
+++ lib/home/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
